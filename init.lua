6193ce9de56075db92ea870942e96e81475b2562
Gerard Geer <gerarddgeer@gmail.com>
2016-08-05 06:18:04 -0500

    Added some stuff I liked from NP++.

---
+++ init.lua
@@ -0,0 +1,33 @@
+-- Set the theme to dark.
+ui.set_theme(not CURSES and 'dark' or 'term', {font = 'Monospace', back='0x000000', fontsize = 8})
+
+-- Use autocomplete on all of multiple selections, not just the first.
+buffer.auto_c_multi = buffer.MULTIAUTOC_EACH
+
+-- Allow multiple carets.
+buffer.multiple_selection = true
+
+-- Allow pasting into each of multiple selections.
+buffer.multi_paste = buffer.MULTIPASTE_EACH
+
+-- Set the tab width to the standard 4.
+buffer.tab_width = 4
+
+-- Space those lines a bit better.
+buffer.extra_ascent = 2
+
+-- Assign line cut to ctrl+l
+keys["cl"] = buffer.line_cut
+
+-- Assign moving lines to ctrl-up/down.
+keys["cup"] = buffer.move_selected_lines_up
+keys["cdown"] = buffer.move_selected_lines_down
+
+-- Set scrolling to ctrl-shift-up/down.
+keys["csup"] = buffer.line_scroll_up
+keys["csdown"] = buffer.line_scroll_down
+
+-- Scrolling past the last line is the best.
+buffer.end_at_last_line = false
+
+end
